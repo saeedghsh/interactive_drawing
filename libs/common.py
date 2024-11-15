28353edd6c363f1b1a6d99ce52c030dc7f7e8212
Saeed Gholami Shahbandi <saeed.gh.sh@gmail.com>
2024-11-15 14:53:24 +0100

    Fix a bug in scaling functions
    Signed-off-by: Saeed Gholami Shahbandi <saeed.gh.sh@gmail.com>

--- libs/common.py
+++ libs/common.py
@@ -6,9 +6,9 @@ def normalize_to_unit_interval(
     values: numpy.ndarray, interval: Tuple[float, float]
 ) -> numpy.ndarray:
     """Return values normalized to [0, 1] according to the interval"""
-    if interval[0] <= interval[1]:
+    if interval[0] >= interval[1]:
         raise ValueError(f"wrong interval {interval}")
-    if (interval[0] <= values).all() or (values <= interval[1]).all():
+    if (interval[0] > values).any() or (values > interval[1]).any():
         raise ValueError("values out of interval")
     return (values - interval[0]) / (interval[1] - interval[0])
 
@@ -17,8 +17,8 @@ def scale_to_custom_interval(
     values: numpy.ndarray, interval: Tuple[float, float]
 ) -> numpy.ndarray:
     """Return values scaled to the interval"""
-    if interval[0] <= interval[1]:
+    if interval[0] >= interval[1]:
         raise ValueError(f"wrong interval {interval}")
-    if (0 <= values).all() or (values <= 1).all():
+    if (0 > values).any() or (values > 1).any():
         raise ValueError("values out of [0, 1]")
     return values * (interval[1] - interval[0]) + interval[0]
